09abf52f480716f032fe0a6977973aad5ec5ea57
Sergeev34429 <149881683+Sergeev34429@users.noreply.github.com>
2024-04-23 20:28:02 +0500

    M4L2

---
+++ M3L4/meme-creator-main/main.py
@@ -0,0 +1,49 @@
+#Импорт
+from flask import Flask, render_template, request, send_from_directory
+
+
+app = Flask(__name__)
+
+#Результаты формы
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # получаем выбранное изображение
+        selected_image = request.form.get('image-selector')
+
+        # Получаем текст
+        text_top = request.form.get('textTop')
+        text_bottom = request.form.get('textBottom')
+        
+        # Получаем расположение текста
+        text_top_y = request.form.get('textTop_y')
+        text_bottom_y = request.form.get('textBottom_y')
+       
+        # Получаем цвет текста
+        selected_color = request.form.get('color-selector')
+        
+        return render_template('index.html', 
+                               # отображаем выбранное изображение
+                               selected_image=selected_image, 
+
+                               # Отображаем текст
+                               text_top = text_top,
+                               text_bottom = text_bottom,
+                               
+                               # Отображаем цвет 
+                               text_top_y = text_top_y,
+                               text_bottom_y = text_bottom_y,
+                               
+                               # Отоброжаем расположение текста
+                               selected_color = selected_color
+                               )
+    else:
+        # отображаем первое изображение по умолчанию
+        return render_template('index.html', selected_image='logo.svg')
+
+
+@app.route('/static/img/<path:path>')
+def serve_images(path):
+    return send_from_directory('static/img', path)
+
+app.run(debug=True)
